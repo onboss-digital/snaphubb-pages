d7245455e20239ba0f6a86d593d4097888024cd3
marketing-onboss <marketingonboss@gmail.com>
2025-11-13 04:01:32 -0300

    correção de tradução br
    Added additional payment-related strings for user interface.

--- resources/lang/br/payment.php
+++ resources/lang/br/payment.php
@@ -56,4 +56,12 @@ return [
     'success' => 'Pagamento aprovado!',
     'cancel' => 'Cancelar',
     'pix_cta' => 'Gerar PIX',
+    'secure_payment' => 'Pagamento 100% seguro com criptografia SSL.',
+    'certified_by' => 'Certificado por Cloudflare Secure Gateway.',
+    'terms_of_use' => 'Termos de Uso',
+    'privacy_policy' => 'Política de Privacidade',
+    'support' => 'Suporte',
+    'copyright' => '2025© Snaphubb. Todos os direitos reservados.',
+    'legal_notice' => 'Aviso Legal',
 ];
+
