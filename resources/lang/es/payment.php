7c1a68876d5984a5e8f5a5a2942e877b07c60002
marketing-onboss <marketingonboss@gmail.com>
2025-11-13 04:06:11 -0300

    correção de tradução es

--- resources/lang/es/payment.php
+++ resources/lang/es/payment.php
@@ -56,4 +56,10 @@ return [
     'success' => '¡Pago aprobado!',
     'cancel' => 'Cancelar',
     'pix_cta' => 'Generar PIX',
+    'secure_payment' => 'Pago 100% seguro con encriptación SSL.',
+    'certified_by' => 'Certificado por Cloudflare Secure Gateway.',
+    'terms_of_use' => 'Términos de Uso',
+    'privacy_policy' => 'Política de Privacidad',
+    'support' => 'Soporte',
+    'copyright' => '© :year Snaphubb. Todos los derechos reservados.',
 ];
